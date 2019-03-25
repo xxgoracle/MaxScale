99e4673f2dc70b81b959ef3c2a4435345a5d2584
Timofey Turenko <timofey.turenko@mariadb.com>
2019-03-25 14:23:44 +0200

    add Maxscale restart to cache bash test

--- maxscale-system-test/cache_basic.sh
+++ maxscale-system-test/cache_basic.sh
@@ -5,5 +5,5 @@ export src_dir=`dirname $rp`
 export test_dir=`pwd`
 export script_name=`basename $rp`
 
-$test_dir/non_native_setup $1 ${script_name}
+$test_dir/non_native_setup $1 ${script_name} --no-maxscale-start
 
