cbee877a1021f37a6bcbb7a7f923c8530e61d834
Timofey Turenko <timofey.turenko@mariadb.com>
2019-04-04 21:30:43 +0300

    fix test name reading

--- maxscale-system-test/labels_table.cpp
+++ maxscale-system-test/labels_table.cpp
@@ -8,13 +8,11 @@ std::string get_mdbci_lables(const char *labels_string)
     std::string mdbci_labels("MAXSCALE");
     for (size_t i = 0; i < sizeof(labels_table) / sizeof(labels_table_t); i++)
     {
-        printf("%lu\t %s\n", i, labels_table[i].test_label.c_str());
-        std::string test_label = std::string(" ") + labels_table[i].test_label;
+        std::string test_label = std::string(";") + labels_table[i].test_label;
         if (strstr(labels_string, test_label.c_str()))
         {
             mdbci_labels += "," + labels_table[i].mdbci_label;
         }
     }
-    printf("mdbci labels %s\n", mdbci_labels.c_str());
     return mdbci_labels;
 }
