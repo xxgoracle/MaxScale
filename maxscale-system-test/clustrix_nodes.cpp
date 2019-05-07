93b7855b8770170b97ec7089483ea00009446e9d
Timofey Turenko <timofey.turenko@mariadb.com>
2019-05-07 17:16:28 +0300

    apply uncrysify to clustrix* files

--- maxscale-system-test/clustrix_nodes.cpp
+++ maxscale-system-test/clustrix_nodes.cpp
@@ -6,7 +6,7 @@
 int Clustrix_nodes::install_clustrix(int m)
 {
     int ec;
-    char * clustrix_rpm = ssh_node_output(m, "rpm -qa | grep clustrix-clxnode", true, &ec);
+    char* clustrix_rpm = ssh_node_output(m, "rpm -qa | grep clustrix-clxnode", true, &ec);
     if (strstr(clustrix_rpm, "clustrix-clxnode") == NULL)
     {
         printf("%s\n", ssh_node_output(m, CLUSTRIX_DEPS_YUM, true, &ec));
@@ -14,7 +14,7 @@ int Clustrix_nodes::install_clustrix(int m)
         printf("%s\n", ssh_node_output(m, UNPACK_CLUSTRIX, false, &ec));
         printf("%s\n", ssh_node_output(m, INSTALL_CLUSTRIX, false, &ec));
         create_users(m);
-   }
+    }
     return 0;
 }
 
@@ -24,8 +24,8 @@ int Clustrix_nodes::start_cluster()
     {
         install_clustrix(i);
     }
-    std::string lic_filename = std::string(getenv("HOME")) +
-            std::string("/.config/mdbci/clustrix_license");
+    std::string lic_filename = std::string(getenv("HOME"))
+        + std::string("/.config/mdbci/clustrix_license");
     std::ifstream lic_file;
     lic_file.open(lic_filename.c_str());
     std::stringstream strStream;
@@ -35,14 +35,14 @@ int Clustrix_nodes::start_cluster()
 
     execute_query_all_nodes(clustrix_license.c_str());
 
-    std::string cluster_setup_sql = std::string("ALTER CLUSTER ADD '") +
-            std::string(IP_private[1]) +
-            std::string("'");
+    std::string cluster_setup_sql = std::string("ALTER CLUSTER ADD '")
+        + std::string(IP_private[1])
+        + std::string("'");
     for (int i = 2; i < N; i++)
     {
-        cluster_setup_sql += std::string(",'") +
-                std::string(IP_private[i]) +
-                std::string("'");
+        cluster_setup_sql += std::string(",'")
+            + std::string(IP_private[i])
+            + std::string("'");
     }
     connect();
     execute_query(nodes[0], "%s", cluster_setup_sql.c_str());
@@ -55,14 +55,14 @@ std::string Clustrix_nodes::cnf_servers()
     std::string s;
     for (int i = 0; i < N; i++)
     {
-        s += std::string("\\n[") +
-                cnf_server_name +
-                std::to_string(i + 1) +
-                std::string("]\\ntype=server\\naddress=") +
-                std::string(IP_private[i]) +
-                std::string("\\nport=") +
-                std::to_string(port[i]) +
-                std::string("\\nprotocol=MySQLBackend\\n");
+        s += std::string("\\n[")
+            + cnf_server_name
+            + std::to_string(i + 1)
+            + std::string("]\\ntype=server\\naddress=")
+            + std::string(IP_private[i])
+            + std::string("\\nport=")
+            + std::to_string(port[i])
+            + std::string("\\nprotocol=MySQLBackend\\n");
     }
     return s;
 }
--- maxscale-system-test/clustrix_nodes.h
+++ maxscale-system-test/clustrix_nodes.h
@@ -17,33 +17,35 @@
 #include "mariadb_nodes.h"
 
 #define CLUSTRIX_DEPS_YUM "yum install -y bzip2 wget screen ntp ntpdate vim htop mdadm"
-#define WGET_CLUSTRIX "wget http://files.clustrix.com/releases/software/clustrix-9.1.4.el7.tar.bz2"
-#define UNPACK_CLUSTRIX "tar xvjf clustrix-9.1.4.el7.tar.bz2"
-#define INSTALL_CLUSTRIX "cd clustrix-9.1.4.el7; sudo ./clxnode_install.py --yes --force"
+#define WGET_CLUSTRIX     "wget http://files.clustrix.com/releases/software/clustrix-9.1.4.el7.tar.bz2"
+#define UNPACK_CLUSTRIX   "tar xvjf clustrix-9.1.4.el7.tar.bz2"
+#define INSTALL_CLUSTRIX  "cd clustrix-9.1.4.el7; sudo ./clxnode_install.py --yes --force"
 
 class Clustrix_nodes : public Mariadb_nodes
 {
 public:
 
-     Clustrix_nodes(const char *pref, const char *test_cwd, bool verbose, std::string network_config) :
-        Mariadb_nodes(pref, test_cwd, verbose, network_config) { }
+    Clustrix_nodes(const char* pref, const char* test_cwd, bool verbose, std::string network_config)
+        : Mariadb_nodes(pref, test_cwd, verbose, network_config)
+    {
+    }
 
-     /**
-      * @brief install_clustrix
-      * @param m node index
-      * @return 0 in case of success
-      */
-     int install_clustrix(int m);
+    /**
+     * @brief install_clustrix
+     * @param m node index
+     * @return 0 in case of success
+     */
+    int install_clustrix(int m);
 
-     /**
-      * @brief start_cluster Intstalls Clustrix on all nodes, configure license, form cluster
-      * @return 0 in case of success
-      */
-     int start_cluster();
+    /**
+     * @brief start_cluster Intstalls Clustrix on all nodes, configure license, form cluster
+     * @return 0 in case of success
+     */
+    int start_cluster();
 
-     /**
-      * @brief cnf_servers Generate Clustrix servers description for maxscale.cnf
-      * @return text for maxscale.cnf
-      */
-     std::string cnf_servers();
+    /**
+     * @brief cnf_servers Generate Clustrix servers description for maxscale.cnf
+     * @return text for maxscale.cnf
+     */
+    std::string cnf_servers();
 };
